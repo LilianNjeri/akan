1f2aecbf947695c601addea14a08ab0394485e45
Lilian Njeri <lilynjeri142@gmail.com>
2022-03-04 18:57:28 +0300

    array days of the week, male and female akan names

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 // main function
 function akanNames() {
+    // user generated input
     var gender = document.getElementById('gender').value
-   var birthDay = document.getElementById('birthday').value
+    var birthDay = document.getElementById('birthday').value
+
+    //array days of the week and male and female akan names
+    const akanArray = [
+        ['Sunday', 'Kwasi', 'Akosua'],
+        ['Monday', 'Kwado', 'Adowa' ],
+        ['Tuesday','Kwabena', 'Abenaa'],
+        ['Wednesday', 'Kwaku', 'Akua'],
+        ['Thursday', 'Yaw' ,'Yaa'],
+        ['Friday', 'Kofi' ,'Afua'],
+        ['Saturday', 'Kwame', 'Ama'],
+    ];
 }
